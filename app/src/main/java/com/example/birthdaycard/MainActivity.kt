54c3313ca296477cef33f272ffaba8771940af09
bvuongle <ducbinhhn2002@gmail.com>
2023-08-04 10:59:20 +0200

    Arrange element

--- app/src/main/java/com/example/birthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,10 +30,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             BirthdayCardTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    GreetingImage(notification = "Yo, It's your birthday",
-                        message = "Rise and Shine, babe",
-                        from = "bvuongle")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingImage(
+                        notification = stringResource(R.string.happy_birthday_text),
+                        message = stringResource(R.string.msg_1),
+                        from = stringResource(R.string.author_1)
+                    )
                 }
             }
         }
@@ -56,13 +62,13 @@ fun GreetingText(notification: String, message: String, from: String, modifier: 
             fontSize = 50.sp,
             textAlign = TextAlign.Center,
             lineHeight = 55.sp,
-            modifier = modifier.padding(8.dp)
+            modifier = Modifier.padding(8.dp)
         )
         Text(
-            text = "From $from",
+            text = stringResource(R.string.from_msg, from),
             fontSize = 30.sp,
             lineHeight = 35.sp,
-            modifier = modifier
+            modifier = Modifier
                 .padding(16.dp)
                 .align(alignment = Alignment.End)
         )
@@ -71,15 +77,19 @@ fun GreetingText(notification: String, message: String, from: String, modifier: 
 
 @Composable
 fun GreetingImage(notification: String, message: String, from: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.androidparty)
-    Box{
+    Box(modifier) {
         Image(
-            painter = image,
+            painter = painterResource(R.drawable.androidparty),
             contentDescription = null,
             contentScale = ContentScale.Crop,
             alpha = 0.75f
         )
-        GreetingText(notification = notification, message = message, from = from, modifier = Modifier.padding(8.dp))
+        GreetingText(
+            notification = notification,
+            message = message,
+            from = from,
+            modifier = Modifier.align(alignment = Alignment.Center).padding(8.dp)
+        )
     }
 }
 
@@ -91,9 +101,9 @@ fun GreetingImage(notification: String, message: String, from: String, modifier:
 fun BirthdayCardPreview() {
     BirthdayCardTheme {
         GreetingImage(
-            notification = "Yooo, It's your birthday",
-            message = "Rise and Shine, babe",
-            from = "bvuongle"
+            notification = stringResource(id = R.string.happy_birthday_text),
+            message = stringResource(id = R.string.msg_1),
+            from = stringResource(id = R.string.author_1)
         )
     }
 }
